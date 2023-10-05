765969fc92b7b73923387553867b7a7d0dc5789d
Fabian Braun <fsbraun@gmx.de>
2023-10-05 22:32:40 +0200

    Fix: Add debug toolbar path correctly

--- backend/urls.py
+++ backend/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
-    urlpatterns.extend(path("__debug__/", include(debug_toolbar.urls)))
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
 
 urlpatterns.append(path('', include('cms.urls')))
 
