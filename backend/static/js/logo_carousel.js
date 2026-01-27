c8dbc47b54e7fb039fcbd988003771104ff37926
theShinigami <salimabdellanuru@gmail.com>
2026-01-27 19:43:40 +0300

    refactor: checks if Swiper is loaded, wrapped in try/catch.

--- backend/static/js/logo_carousel.js
+++ backend/static/js/logo_carousel.js
@@ -7,40 +7,51 @@
 	'use strict';
 
 	function initSwiper() {
-		const swiperElements = document.querySelectorAll('.swiper');
 
-		swiperElements.forEach(elem => {
-			const loop = (elem.dataset.loop) === 'true';
-			const spaceBetweenSlides = parseInt(elem.dataset.spaceBetweenSlides);
-			const autoplay = (elem.dataset.autoplay === 'true')
-				? { delay: parseInt(elem.dataset.delay), disableOnInteraction: false }
-				: false;
+		if (typeof Swiper === 'undefined') {
+			console.error('Swiper library not loaded!');
+			return;
+		}
 
-			new Swiper(`.logoSwiper-${elem.dataset.instanceId}`, {
-				slidesPerView: 1,      // Mobile
-				spaceBetween: spaceBetweenSlides,
-				loop: loop,
-				keyboard: {
-					enabled: true,
-					onlyInViewport: true,
-				},
-				autoplay: autoplay,
-				navigation: {
-					nextEl: `.btn-next-${elem.dataset.instanceId}`,
-					prevEl: `.btn-prev-${elem.dataset.instanceId}`,
-				},
-				breakpoints: {
-				    768: {
-					slidesPerView: 3, // Tablet
-					spaceBetween: 40
-				    },
-				    1024: {
-					slidesPerView: 4, // Desktop
-					spaceBetween: 60
-				    }
-				}
+		try {
+			const swiperElements = document.querySelectorAll('.swiper');
+
+			swiperElements.forEach(elem => {
+				const loop = (elem.dataset.loop) === 'true';
+				const spaceBetweenSlides = parseInt(elem.dataset.spaceBetweenSlides) || 20;
+				const delay = parseInt(elem.dataset.delay) || 3000;
+				const autoplay = (elem.dataset.autoplay === 'true')
+					? {delay: delay, disableOnInteraction: false}
+					: false;
+
+				new Swiper(`.logoSwiper-${elem.dataset.instanceId}`, {
+					slidesPerView: 1,      // Mobile
+					spaceBetween: spaceBetweenSlides,
+					loop: loop,
+					keyboard: {
+						enabled: true,
+						onlyInViewport: true,
+					},
+					autoplay: autoplay,
+					navigation: {
+						nextEl: `.btn-next-${elem.dataset.instanceId}`,
+						prevEl: `.btn-prev-${elem.dataset.instanceId}`,
+					},
+					breakpoints: {
+						768: {
+							slidesPerView: 3, // Tablet
+							spaceBetween: 40
+						},
+						1024: {
+							slidesPerView: 4, // Desktop
+							spaceBetween: 60
+						}
+					}
+				});
 			});
-		});
+		} catch (e) {
+			console.error("Error initializing Swiper instance", e);
+		}
 	}
 
 	if (document.readyState === 'loading') {
