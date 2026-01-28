3febc8766fee7020d3be9b200252c546f30e2077
maxnoelp2 <max@surfgreen.dev>
2026-01-28 11:05:05 +0100

    Feat/timeline plugin (#23)

--- cms_theme/cms_components.py
+++ cms_theme/cms_components.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from djangocms_frontend.component_base import CMSFrontendComponent
 from djangocms_frontend.component_pool import components
+from djangocms_frontend.fields import ColoredButtonGroup
 
 
 @components.register
@@ -9,7 +11,7 @@ class Hero(CMSFrontendComponent):
     """Hero component with background grid option"""
 
     class Meta:
-        plugin_name = _("Hero")
+        name = _("Hero")
         render_template = "hero/hero.html"
         allow_children = True
         child_classes = [
@@ -26,7 +28,6 @@ class Hero(CMSFrontendComponent):
         required=False,
         initial=False,
     )
-
 
 @components.register
 class Features(CMSFrontendComponent):
@@ -49,7 +50,7 @@ class Features(CMSFrontendComponent):
         required=False,
         initial=False,
     )
-
+  
     mirror_layout = forms.BooleanField(
         label=_("Mirror layout"),
         required=False,
@@ -70,4 +71,41 @@ class Features(CMSFrontendComponent):
         ],
         required=False,
         initial="default",
+
+@components.register
+class TimelineContainer(CMSFrontendComponent):
+    """Timeline component with vertical layout option"""
+
+    class Meta:
+        name = _("Timeline")
+        render_template = "timeline/timeline.html"
+        allow_children = True
+        child_classes = [
+            "CardPlugin",
+            "TextPlugin",
+            "HeadingPlugin",
+            "SpacingPlugin",
+        ]
+        mixins = [
+            "Background",
+            "Spacing",
+            "Attributes",
+        ]
+        
+    divider_color = forms.ChoiceField(
+        label=_("Divider line color"),
+        choices=settings.DJANGOCMS_FRONTEND_COLOR_STYLE_CHOICES,
+        required=False,
+        initial="primary",
+        help_text=_("Color of the vertical timeline line."),
+        widget=ColoredButtonGroup(attrs={"class": "flex-wrap"}),
+    )
+
+    circle_color = forms.ChoiceField(
+        label=_("Circle color"),
+        choices=settings.DJANGOCMS_FRONTEND_COLOR_STYLE_CHOICES,
+        required=False,
+        initial="secondary",
+        help_text=_("Color of the timeline circles."),
+        widget=ColoredButtonGroup(attrs={"class": "flex-wrap"}),
     )
